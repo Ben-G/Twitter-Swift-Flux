194fb623b9c339790cc431958a20b36334ff7c2f
Ben-G <Benjamin.Encz@gmail.com>
2015-01-24 23:44:59 -0800

    Initial Commit

---
+++ TwitterLivestreamSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterLivestreamSwift
+//
+//  Created by Benjamin Encz on 1/24/15.
+//  Copyright (c) 2015 Benjamin Encz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
